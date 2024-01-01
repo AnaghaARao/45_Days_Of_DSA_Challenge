b39cc9431a469aeb009afa6404ec849c6c4634d9
AnaghaARao <rao.anagha2002@gmail.com>
2024-01-01 18:49:30 +0530

    Input Function Implementation

--- LINKED LIST-1/linkedList.cpp
+++ LINKED LIST-1/linkedList.cpp
@@ -14,25 +14,34 @@ class Node{
         
 };
 
+Node* takeInput(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    int ele;
+    cin>> ele;
+    while(ele != -1){
+        Node *temp = new Node(ele);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+        }else{
+            tail->next = temp;
+            tail = tail->next;
+        }
+        cin>>ele;
+    } 
+    return head;
+}
+
 void print(Node *head){
     Node *temp = head;
     while(temp!=NULL){
-        cout<< temp->data;
+        cout<< temp->data<<" ";
         temp = temp->next;
     }
 }
 
 int main(){
-    Node n1(1);
-    Node n2(2);
-    Node n3(3);
-    Node n4(4);
-    Node n5(5);
-    Node *head = &n1;
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = &n4;
-    n4.next = &n5;
-
+    Node *head = takeInput();
     print(head);
 }
