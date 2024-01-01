35d62d18a479f07d30e8eae121b61a3cf1f98811
AnaghaARao <rao.anagha2002@gmail.com>
2024-01-01 18:59:57 +0530

    Length of LinkedList Function Implementation

---
+++ LINKED LIST-1/lengthOfLL.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node()
+        {
+            this->data = 0;
+            next = NULL;
+        }
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(int data, Node* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+};
+
+int length(Node *head)
+{
+	Node *temp = head;
+    int cnt = 1; // cause there is something in the head for sure
+    while(temp->next!=NULL){ // ensures that -1 isnt included in the count for length of LL
+        cnt+=1;
+        temp = temp->next;
+    }
+    return cnt;
+    // terminator in this case is -1....hence not to be included in length....taken care of by the while loop condition
+}
