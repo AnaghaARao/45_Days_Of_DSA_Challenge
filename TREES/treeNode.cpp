cf3dfca74d887be61bd6be209f27fe137e733a95
AnaghaARao <rao.anagha2002@gmail.com>
2024-01-17 07:41:37 +0530

    Implemented takeInput and printTree level wise

--- TREES/treeNode.cpp
+++ TREES/treeNode.cpp
@@ -2,6 +2,33 @@
 #include "TreeNode.h"
 using namespace std;
 
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout<<"Enter root data: "<<endl;
+    cin>>rootData;
+
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(!pendingNodes.empty()){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout<<"Enter no. of children of "<< front->data;
+        int numChild;
+        cin>>numChild;
+        for(int i = 0 ; i < numChild ; i++){
+            int childData;
+            cout<<"Enter "<< i <<"th child of "<< front->data << endl;
+            cin>>childData;
+            TreeNode<int>* child = new TreeNode<int>(childData); // dynamic allocation to ensure that memory isnt lost
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+
 TreeNode<int>* takeInput() {
     int rootData;
     cout<< "Enter data\n";
@@ -15,6 +42,24 @@ TreeNode<int>* takeInput() {
         root->children.push_back(child);
     }
     return root;
+}
+
+void printTreeLevelWise(TreeNode<int>* root){
+    if(root == NULL) return;
+    queue<TreeNode<int>*> pending;
+    pending.push(root);
+    while(!pending.empty()){
+        TreeNode<int>* front = pending.front();
+        pending.pop(); 
+        cout<< front->data<<":";
+        for(int i = 0 ; i < front->children.size() ; i++){
+            if(i==0) cout<< front->children.at(i)->data;
+            else cout<<","<<front->children.at(i)->data;
+            pending.push(front->children.at(i));
+        }
+        cout<<endl;
+    }
+
 }
 
 void printTree(TreeNode<int>* root){
@@ -38,7 +83,7 @@ int main(){
     // root->children.push_back(node1);
     // root->children.push_back(node2);
 
-    TreeNode<int>* root = takeInput();
-    printTree(root);
+    TreeNode<int>* root = takeInputLevelWise();
+    printTreeLevelWise(root);
     // ToDo delete the tree
 }
