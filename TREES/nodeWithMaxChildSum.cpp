213535ee393b666f807e16ef967c3a673db09225
AnaghaARao <rao.anagha2002@gmail.com>
2024-02-09 18:56:20 +0530

    Node with max child sum

--- TREES/nodeWithMaxChildSum.cpp
+++ TREES/nodeWithMaxChildSum.cpp
@@ -1,5 +1,6 @@
 /*
-Given a generic tree, find and return the node for which sum of its data and data of all its child nodes is maximum. In the sum, data of the node and data of its immediate child nodes has to be taken.
+Given a generic tree, find and return the node for which sum of its data and data of all its child nodes is maximum. 
+In the sum, data of the node and data of its immediate child nodes has to be taken.
 */
 
 #include<bits/stdc++.h>
