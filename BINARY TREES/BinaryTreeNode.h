7027201f15ef4dbdc9ef8d4151a1ed0bf05f2a20
AnaghaARao <rao.anagha2002@gmail.com>
2024-02-25 13:30:28 +0530

    BST header file

---
+++ BINARY TREES/BinaryTreeNode.h
@@ -0,0 +1,23 @@
+/*
+    Binary tree is a type of tree where every node has a maximum of 2 children
+    The binary tree node class needs 3 data members: data, left_node, right_node
+*/
+
+template <typename T>
+class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode* left;
+        BinaryTreeNode* right;
+
+        BinaryTreeNode(T data){
+            this->data = data;
+            right = NULL;
+            left = NULL;
+        }
+
+        ~BinaryTreeNode(){
+            delete left;
+            delete right;
+        }
+}
