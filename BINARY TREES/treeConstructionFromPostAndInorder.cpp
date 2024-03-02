bcc1c4c0aaeb8228e2ac0ab549cbe6a8969fe32a
AnaghaARao <rao.anagha2002@gmail.com>
2024-03-02 21:43:48 +0530

    Binary Tree costruction

---
+++ BINARY TREES/treeConstructionFromPostAndInorder.cpp
@@ -0,0 +1,16 @@
+/*
+Problem statement
+For a given postorder and inorder traversal of a Binary Tree of type integer stored in an array/list, 
+create the binary tree using the given two arrays/lists. 
+You just need to construct the tree and return the root.
+Note:
+Assume that the Binary Tree contains only unique elements.
+*/
+
+#include<bits/stdc++.h>
+#include"BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength) {
+    // Write your code here
+}
