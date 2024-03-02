1c17c7254d86ec2d85ac9b30a0683684360f57d4
AnaghaARao <rao.anagha2002@gmail.com>
2024-03-02 21:59:16 +0530

    Binary tree construction using Post and Inorder traversal

--- BINARY TREES/treeConstructionFromPostAndInorder.cpp
+++ BINARY TREES/treeConstructionFromPostAndInorder.cpp
@@ -12,5 +12,23 @@ Assume that the Binary Tree contains only unique elements.
 using namespace std;
 
 BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength) {
-    // Write your code here
+
+ if(postLength == 0 || inLength == 0)   {
+    return nullptr;
+ }
+
+    BinaryTreeNode<int>* root  = new BinaryTreeNode<int>(postorder[postLength-1]);
+
+    int rootIndex;
+    for(int i = 0 ; i < inLength ; i++){
+        if(inorder[i] == postorder[postLength-1]){
+            rootIndex = i;
+            break;
+        }
+    }
+
+    root->left = buildTree(postorder , rootIndex , inorder, rootIndex);
+    root->right = buildTree(postorder + rootIndex , postLength - rootIndex - 1 , inorder + rootIndex + 1 , inLength - rootIndex - 1);
+
+    return root;
 }
