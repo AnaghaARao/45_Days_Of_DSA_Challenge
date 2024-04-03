8d99f000c2bceb01685a92f27bf0d43f723e38a5
AnaghaARao <rao.anagha2002@gmail.com>
2024-04-03 21:28:57 +0530

    Leetcode 2433

---
+++ ARRAYS/originalArrayOfPrefixXOR.cpp
@@ -0,0 +1,20 @@
+/* You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+Note that ^ denotes the bitwise-xor operation.
+It can be proven that the answer is unique. */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> res;
+        res.push_back(pref[0]);
+
+        for(int i = 1 ; i < pref.size() ; i++){
+            res.push_back(pref[i-1]^pref[i]);
+        }
+        return res;
+    }
+};
