9bf7d7caa82cf66f29bd50799aa6c3d18aca3eae
AnaghaARao <rao.anagha2002@gmail.com>
2024-04-18 22:04:28 +0530

    Min & Max value in BT

--- BINARY TREES/minMaxInBinaryTree.cpp
+++ BINARY TREES/minMaxInBinaryTree.cpp
@@ -28,22 +28,20 @@
 using namespace std;
 
 pair<int, int> getMinAndMax(BinaryTreeNode<int> *root) {
-    if(root == NULL){
-        pair<int, int> res;
-        res.first = 0;
-        res.second = 0;
-        return res;
+    // VERY IMPORTANT - HOW WE RETURN THE VALUES WHEN ENCOUNTERING NULL ROOT
+    // A really cool method!!
+    if (root == nullptr) {
+        // Return the extremes as the base case.
+        return {std::numeric_limits<int>::max(), std::numeric_limits<int>::min()};
     }
 
-    pair<int, int> res;
-    res.first = root->data;
-    res.second = root->data;
+    pair<int, int> res{root->data, root->data};
 
     pair<int, int> left = getMinAndMax(root->left);
     pair<int, int> right = getMinAndMax(root->right);
 
-    res.first = min(left.first, right.first, res.first);
-    res.second = max(left.second, right.second, res.second);
+    res.first = min({left.first, right.first, res.first});
+    res.second = max({left.second, right.second, res.second});
 
     return res;
 }
