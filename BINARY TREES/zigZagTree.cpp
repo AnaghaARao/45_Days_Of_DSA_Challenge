a1c23ac13e7e36c735a4c4f2e6ec26d3a2dc93c8
AnaghaARao <rao.anagha2002@gmail.com>
2024-04-25 09:27:27 +0530

    zig Zag Tree Traversal [H]

--- BINARY TREES/zigZagTree.cpp
+++ BINARY TREES/zigZagTree.cpp
@@ -18,19 +18,52 @@ class Node {
     }
 };
 
-void zigZagOrder(BinaryTreeNode<int> *root) {
-    if(root == nullptr) return;
-
-    queue<BinaryTreeNode<int>*> pendingNodes;
-    pendingNodes.push(root);
-    int flag = 1;
-
-    while(! pendingNodes.empty()){
-
-        Node<int>* levelHead = nullptr;
-        Node<int>* levelTail = nullptr;
-        int levelSize = pendingNodes.size();
-
-        for(int i = 0 ; i < )
+void printZigZag(vector<vector<int>> tree){
+    int n = tree.size();
+    for(int i = 0 ; i < n ; i++){
+        int m = tree[i].size();
+        for(int j = 0 ; j < m ; j++){
+            cout<<tree[i][j]<<"\t";
+        }
+        cout<<"\n";
     }
+    return;
+}
+
+void zigZagOrder(BinaryTreeNode<int> *root) {
+    
+    // base case
+    if(root == nullptr){
+        return;
+    }
+
+    // initialization
+    vector<vector<int>> res;
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    bool leftToRight = true;
+
+    pendingNodes.push(root);
+
+    while(!pendingNodes.empty()){
+        int levelWiseSize = pendingNodes.size();
+        vector<int> row(levelWiseSize);
+        
+        for(int i = 0 ; i < levelWiseSize ; i++){
+            // find front for each iteration
+            BinaryTreeNode<int>* front = pendingNodes.front();
+            pendingNodes.pop();
+            
+            // VERY IMPORTANT STEP:
+            int index = (leftToRight) ? i : (levelWiseSize-i-1);
+            row[index] = front->data;
+
+            if(front->left) pendingNodes.push(front->left);
+            if(front->right) pendingNodes.push(front->right);
+        }
+
+        leftToRight = !leftToRight;
+        res.push_back(row);
+    }
+    printZigZag(res);
+    return;
 }
