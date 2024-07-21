a1be5c282edb424aabb2025308080f837af1ee88
AnaghaARao <rao.anagha2002@gmail.com>
2024-07-21 17:40:10 +0530

    Leetcode 1315[M]

--- BINARY TREES/sumOfNodesWithEvenValuedGrandparent.cpp
+++ BINARY TREES/sumOfNodesWithEvenValuedGrandparent.cpp
@@ -1,3 +1,7 @@
+// Given the root of a binary tree, return the sum of values of nodes with an even-valued grandparent. 
+// If there are no nodes with an even-valued grandparent, return 0.
+// A grandparent of a node is the parent of its parent if it exists.
+
 #include<bits/stdc++.h>
 using namespace std;
 
