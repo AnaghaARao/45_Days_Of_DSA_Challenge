8d0ace475df1cdcdc670c5287cd1d12343db2923
AnaghaARao <rao.anagha2002@gmail.com>
2024-07-22 23:36:41 +0530

    Largest BST[M]

--- BINARY SEARCH TREE/largestBST.cpp
+++ BINARY SEARCH TREE/largestBST.cpp
@@ -10,27 +10,43 @@
 #include "BinarySearchTree.h"
 using namespace std;
 
-bool isBST(BinaryTreeNode<int>* root, int& h){
-    if(root == NULL) return true;
-
-    bool leftFlag = false, rightFlag = false;
-    if(root->left){
-        if(root->left->data <= root->data && isBST(root->left, h)) leftFlag = true;
+class Pair{
+    public:
+    int height;
+    bool check;
+    int minVal;
+    int maxVal;
+    Pair(bool flag, int h, int min, int max){
+        height = h;
+        check = flag;
+        minVal = min;
+        maxVal = max;
     }
 
-    if(root->right){
-        if(root->right->data > root->data && isBST(root->right, h)) rightFlag = true;
+};
+
+Pair isBST(BinaryTreeNode<int>* root){
+
+    if(root == NULL) return Pair(true, 0, INT_MAX, INT_MIN);
+
+    Pair left = isBST(root->left);
+    Pair right = isBST(root->right);
+
+    if(left.check && right.check && root->data > left.maxVal && root->data < right.minVal){ // check if root is part of BST
+        int max = max(root->data, right.maxVal); // update bst max
+        int min = min(root->data, left.minVal); // update bst min
+        int height = max(left.height , right.height) + 1; // increment height of true
+        return Pair(true, height, min, max);
     }
 
-    if(leftFlag && rightFlag){
-        h++;
-        return true;
-    }
+    return Pair(false, max(left.height, right.height), 0, 0); 
+    // if not, reset the min and max value, eturn max of previous BST height and return false
 
-    h--;
-    return false;
 }
 
 int largestBSTSubtree(BinaryTreeNode<int> *root) {
     // logic to find bst from top to bottom
+    if(root == NULL) return 0;
+    Pair res = isBST(root);
+    return res.height;
 }
