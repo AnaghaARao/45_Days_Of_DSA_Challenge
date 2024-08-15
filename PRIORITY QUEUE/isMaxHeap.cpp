3a280b37c9e2e2973a52a45a9b5042ff1bf6a0a2
AnaghaARao <rao.anagha2002@gmail.com>
2024-08-15 23:06:32 +0530

    check max-heap

---
+++ PRIORITY QUEUE/isMaxHeap.cpp
@@ -0,0 +1,12 @@
+// Given an array of integers, check whether it represents max-heap or not. 
+// Return true if the given array represents max-heap, else return false.
+
+bool isMaxHeap(int arr[], int n) {
+    // Write your code here
+    int i = 0 ;
+    while(i<n){
+        if((i*2+1 < n && arr[i] < arr[i*2+1]) || (i*2+2 < n && arr[i]<arr[i*2+2])) return false;
+        i++;
+    }
+    return true;
+}
